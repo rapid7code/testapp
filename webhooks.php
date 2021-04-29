cdc6078e4f7f5501d1732dba3bd313295598fc24
Bac Le Vu <itoutsource17@generali-life.com.vn>
2021-04-29 15:53:01 +0700

    test app

--- webhooks.php
+++ webhooks.php
@@ -5,3 +5,10 @@ echo '<pre>';
 print_r($_POST);
 
 file_put_contents("newfile.txt", $_POST);
+
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+$txt = "John Doe\n";
+fwrite($myfile, $txt);
+$txt = "Jane Doe\n";
+fwrite($myfile, $txt);
+fclose($myfile);

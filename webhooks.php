8eca29548e14fcffc5ec586f4ba5ec259df9a406
Bac Le Vu <itoutsource17@generali-life.com.vn>
2021-04-29 15:57:30 +0700

    test app

--- webhooks.php
+++ webhooks.php
@@ -1,14 +1,17 @@
 <?php
 echo $_REQUEST['hub_challenge'];
 
-echo '<pre>';
-print_r($_POST);
-
-file_put_contents("newfile.txt", $_POST);
+// echo '<pre>';
+// print_r($_POST);
+$arg = [];
+$arg['request'] = $_REQUEST;
+// file_put_contents("newfile.txt", $_POST);
 
 $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
 $txt = "John Doe\n";
 fwrite($myfile, $txt);
 $txt = "Jane Doe\n";
+fwrite($myfile, $txt);
+$txt = implode($arg());
 fwrite($myfile, $txt);
 fclose($myfile);
